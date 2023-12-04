2b7a929818565ad073bac85f654fd8040d3dcb71
Maxime Lottin <maxime.lottin@outlook.com>
2023-12-04 14:35:57 +0100

    Passage de parametre a une méthode

--- MaClass.php
+++ MaClass.php
@@ -1,16 +1,38 @@
 <?php
 
-class Maclass
+class MaClass
 {
-    public $attributPublic;
-    public $couleur = 'vert';
-    public $age = 51;
-    private $_attributPrivate;
-    private $_nom = "Michel PLIK";
+    public string $attributPublic;
+    public string $couleur = 'vert';
+    public int $age = 51;
+    public string $prenom;
+    public const PI = 3.14;
+
+    public function displayMethode($value)
+    {
+        return $this->prenom . ' aime la couleur ' . $this->couleur . ' ' . $value;
+    }
+
+    public function calculAire($rayon) : float | int
+    {
+        return self::PI * $rayon * $rayon;
+    }
+
+    public function calculAire2($rayonParam) : float | int
+    {
+        return self::PI * $rayonParam * $rayonParam;
+    }
 }
 
-$obj = new Maclass();
+$obj = new MaClass();
+$obj->prenom = 'Michel';
 
-echo 'Couleur : ' . $obj->couleur . '<br>';
-echo 'Age : ' . $obj->age . '<br>';
-// echo 'Nom : ' . $obj->_nom . '<br>';
+echo $obj->displayMethode('couleur') . '<br>';
+
+$rayonParam = 5;
+$rayon = 4;
+echo 'L\'aire du cercle de rayon ' . $rayon . ' est : ' . $obj->calculAire($rayon) . '<br>';
+
+$rayonParam = 3;
+echo 'L\'aire du cercle de rayon ' . $rayonParam . ' est : ' . $obj->calculAire2($rayonParam);
+?>
