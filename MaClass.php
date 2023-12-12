651403b390f5556fbcbca2d0083b765b16ab5d0f
Maxime Lottin <maxime.lottin@outlook.com>
2023-12-12 10:55:33 +0100

    Changement dans ma MaClass.php

--- MaClass.php
+++ MaClass.php
@@ -2,10 +2,29 @@
 
 class MaClass
 {
+    public string $attributPublic;
+    public string $couleur = 'vert';
+    public int $age = 51;
+    public string $nom;
+    public string $prenom;
+    public const PI = 3.14;
+    public float | int $rayon;
+    static string $varstatic = 'Je suis une variable statique';
 
-    public function __construct()
+    /*   public function __construct()
     {
         echo 'Je suis une méthode magique (__construct) de la classe : ' . __CLASS__ . ' <br>';
+    } */
+
+    public function __construct($nom, $prenom)
+    {
+        $this->nom = $nom;
+        $this->prenom = $prenom;
+    }
+
+    public function affichePersonne()
+    {
+        echo 'Nom : ' . $this->nom . ', Prénom : ' . $this->prenom . '<br>';
     }
 
     public function __destruct()
@@ -13,25 +32,17 @@ class MaClass
         echo 'Je suis une méthode magique (__destruct) de la classe : ' . __CLASS__ . ' <br>';
     }
 
-    public string $attributPublic;
-    public string $couleur = 'vert';
-    public int $age = 51;
-    public string $prenom;
-    public const PI = 3.14;
-    public float | int $rayon;
-    static string $varstatic = 'Je suis une variable statique';
-
     public function displayMethode()
     {
-        return $this->prenom . ' aime la couleur ' . $this->couleur . ' ' ;
+        return $this->prenom . ' aime la couleur ' . $this->couleur . ' ';
     }
 
-    public function calculAire($rayon) : float | int
+    public function calculAire($rayon): float | int
     {
         return self::PI * $rayon * $rayon;
     }
 
-    public function calculAire2($rayonParam) : float | int
+    public function calculAire2($rayonParam): float | int
     {
         return self::PI * $rayonParam * $rayonParam;
     }
@@ -42,7 +53,8 @@ class MaClass
     }
 }
 
-$obj = new MaClass();
+
+$obj = new MaClass('Carroll', 'Linda');
 $obj->prenom = 'Michel';
 
 echo $obj->displayMethode('couleur') . '<br>';
@@ -54,5 +66,6 @@ echo 'L\'aire du cercle de rayon ' . $rayon . ' est : ' . $obj->calculAire($rayo
 $rayonParam = 3;
 echo 'L\'aire du cercle de rayon ' . $rayonParam . ' est : ' . $obj->calculAire2($rayonParam) . '<br>';
 
+$obj->affichePersonne();
+
 echo MaClass::staticFunc();
-?>
